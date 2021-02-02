4696fcfbf8b0c953beab058f93c07448477a87ca
root <root@ubuntu2010.linuxvmimages.local>
2021-02-02 04:30:03 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Bosulica! Welcome!!! 
-        </p>
+        Saluti!
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
